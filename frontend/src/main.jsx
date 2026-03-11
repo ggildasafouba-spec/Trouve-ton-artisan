cbb7969cd97060f9d49d0f3ceb533e8c2b824086
germainAfouba <ggildasafouba@gmail.com>
2026-03-12 00:09:34 +0100

    flix App import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.scss";
-import App from "./App";
+import App from "./app.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
